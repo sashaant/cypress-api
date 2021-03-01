4461ad9ebc7dbb56321376c14d0cb3fc9e9e8033
sashaant <antonenko.qa@gmail.com>
2021-03-01 21:49:00 +0200

    API Tests : Validate Header

---
+++ cypress/integration/api.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('REST API Test', () =>{
+    it('API TEST - Validate Headers', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.get('@pokemon')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+    })
+})
